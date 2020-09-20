abc5f41db26caabad999f250da18b06b27eb1eed
Ubuntu <ubuntu@ip-172-31-69-165.eu-west-1.compute.internal>
2020-09-20 06:44:53 +0000

    Cleanup SCM references

--- ansible/build/roles/install-jenkins/files/init-userdata.sh
+++ ansible/build/roles/install-jenkins/files/init-userdata.sh
@@ -4,6 +4,8 @@ echo "Preparing configuration"
 envsubst < /etc/jenkins/jenkins-configuration-template.yml | tee /etc/jenkins/jenkins-configuration.yml
 envsubst < /etc/jenkins/gerrit-trigger-template.xml | tee /var/lib/jenkins/gerrit-trigger.xml
 
+chown jenkins:jenkins /var/lib/jenkins -R
+
 echo "Starting jenkins"
 
 systemctl start jenkins
