7ae0114814bdc5dba169902ef1d358bbd95f4413
Ubuntu <ubuntu@ip-172-31-69-165.eu-west-1.compute.internal>
2020-09-20 06:22:22 +0000

    Remove build user

--- build.sh
+++ build.sh
@@ -3,16 +3,16 @@
 set -e 
 
 
-echo "Building build-svc ${BUILD_ID}"
+echo "Building alpha-jenkins-svc ${BUILD_ID}"
 
-IMAGE_ID=$(aws ec2 describe-images --filters "Name=name,Values=build-alpha-build-svc.b${BUILD_ID}" --query 'Images[0].ImageId' --output text)
+IMAGE_ID=$(aws ec2 describe-images --filters "Name=name,Values=build-alpha-jenkins-svc.b${BUILD_ID}" --query 'Images[0].ImageId' --output text)
 echo "Image id ${IMAGE_ID}"
 aws ec2 deregister-image --image-id ${IMAGE_ID} 2> /dev/null | echo "No image existing"
 
 docker build --progress=plain \
              --no-cache \
 	     --build-arg BUILD_ID="${BUILD_ID}" \
-	     -t alpha-build-svc:b${BUILD_ID} \
+	     -t alpha-jenkins-svc:b${BUILD_ID} \
 	     -f Dockerfile .
 
 
--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ echo "Going to deploy me some stuff"
 docker run -it -v /var/run/docker.sock:/var/run/docker.sock \
 	  -e TargetAccountId="${TARGET_ACCOUNT_ID}" \
 	  -e EnvironmentNameUpper="PIPELINE" \
-	  -e ServiceName="alpha-build-svc" \
+	  -e ServiceName="alpha-jenkins-svc" \
 	  -e BUILD_ID="${BUILD_ID}" \
-	  alpha-build-svc:b${BUILD_ID} /dist/deploy.sh
+	  alpha-jenkins-svc:b${BUILD_ID} /dist/deploy.sh
