b666f3f4a11317d5f065230403e2d10ed5a2c719
Ubuntu <ubuntu@ip-172-31-69-165.eu-west-1.compute.internal>
2020-09-23 12:24:13 +0000

    Fix

--- ansible/build/roles/install-jenkins/files/init-userdata.sh
+++ ansible/build/roles/install-jenkins/files/init-userdata.sh
@@ -1,5 +1,15 @@
 #!/bin/bash 
 
+jira_http="$(aws  secretsmanager  get-secret-value \
+	                  --secret-id "/${EnvironmentNameLower}/jenkins/jira-http" \
+	                  --query "SecretString" --output text)"
+
+export JiraHttpUser="$(echo ${jira_http} | jq -r '.username')"
+export JiraHttpPassword="$(echo ${jira_http} | jq -r '.password')"
+
+echo "JiraHttpUser=${JiraHttpUser}" >> /etc/environment
+echo "JiraHttpPassword=${JiraHttpPassword}" >> /etc/environment
+
 echo "Preparing configuration"
 envsubst < /etc/jenkins/jenkins-configuration-template.yml | tee /etc/jenkins/jenkins-configuration.yml
 envsubst < /etc/jenkins/gerrit-trigger-template.xml | tee /var/lib/jenkins/gerrit-trigger.xml
