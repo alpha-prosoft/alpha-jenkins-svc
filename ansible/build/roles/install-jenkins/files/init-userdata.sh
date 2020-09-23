4d5054ea6d5302dbeac348438896fa7302f5dc19
Ubuntu <ubuntu@ip-172-31-69-165.eu-west-1.compute.internal>
2020-09-23 13:10:30 +0000

    Add startup script

--- ansible/build/roles/install-jenkins/files/init-userdata.sh
+++ ansible/build/roles/install-jenkins/files/init-userdata.sh
@@ -10,6 +10,39 @@ export JiraHttpPassword="$(echo ${jira_http} | jq -r '.password')"
 echo "JiraHttpUser=${JiraHttpUser}" >> /etc/environment
 echo "JiraHttpPassword=${JiraHttpPassword}" >> /etc/environment
 
+
+cat << EOF /opt/startup.sh
+set -e
+
+mkdir /home/jenkins/.ssh
+
+aws ssm get-parameter \
+        --name "/pipeline/public/jenkins/.ssh/id_rsa.pub" \
+        --with-decryption \
+        --query 'Parameter.Value' \
+        --output text | base64 -d > /home/jenkins/.ssh/authorized_keys
+chown jenkins:jenkins /home/jenkins/.ssh/authorized_keys
+/opt/login-job.sh
+
+EOF
+chmod +x /opt/startup.sh
+
+cat << EOF /etc/systemd/system/agent-init.service
+[Unit]
+Description=Execute init script
+After=network.target
+
+[Service]
+Type=oneshot
+ExecStart=/opt/startup.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable agent-init.service
+
+
+
 echo "Preparing configuration"
 envsubst < /etc/jenkins/jenkins-configuration-template.yml | tee /etc/jenkins/jenkins-configuration.yml
 envsubst < /etc/jenkins/gerrit-trigger-template.xml | tee /var/lib/jenkins/gerrit-trigger.xml
