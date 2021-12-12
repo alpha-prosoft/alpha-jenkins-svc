43adff63fa2acb7c18f3b3cfe35caff86ed4e8c6
root <root@ip-172-31-68-67.eu-west-1.compute.internal>
2021-12-12 18:03:32 +0000

    Enable buildkit and prod maven settings

--- local-deploy.sh
+++ local-deploy.sh
@@ -2,6 +2,7 @@
 
 export TARGET_ACCOUNT_ID="$(aws sts get-caller-identity | jq -r '.Account')"
 export DOCKER_BUILDKIT=0
+export DOCKER_BUILDKIT=1
 
 export TARGET=ubuntu@${1}
 if [[ "${1:-}" != "" ]]; then
