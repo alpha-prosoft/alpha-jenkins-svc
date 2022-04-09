ed8e86faf9e73cced736091ab3091482cca8107c
Robert Pofuk <robert.pofuk@alpha-prosoft.com>
2022-04-09 22:54:56 +0200

    Downlaod deploy.sh

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ ARG PROJECT_NAME=alpha-jenkins-svc
 
 FROM alphaprosoft/ansible-img:b298
 
+COPY deploy.sh /dist/deploy.sh
+
 ENV BUILD_ID ${BUILD_ID}
 
 
--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -e
 
 echo "Building alpha-jenkins-svc ${BUILD_ID}"
 
+curl https://raw.githubusercontent.com/raiffeisenbankinternational/cbd-jenkins-pipeline/master/ext/deploy.sh > deploy.sh
+
 docker build --progress=plain \
              --no-cache \
 	     --build-arg BUILD_ID="${BUILD_ID}" \
