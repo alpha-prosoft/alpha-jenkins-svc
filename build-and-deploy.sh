78cca7ef653fabc7d83bdf7a2523ea91a841e9a1
Robert Pofuk <rpofuk@gmail.com>
2022-10-26 14:14:41 +0200

    All if fetch in build.sh

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -2,16 +2,4 @@
 
 set -e 
 
-export TARGET_ACCOUNT_ID="$(aws sts get-caller-identity | jq -r '.Account')"
-export DOCKER_BUILDKIT=0
-export DOCKER_BUILDKIT=1
-
-export TARGET=ubuntu@${1}
-if [[ "${1:-}" != "" ]]; then
-  export DOCKER_HOST=ssh://${TARGET}
-fi
-export BUILD_ID="74"
-
-export AWS_DEFAULT_REGION=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
-
 ./build.sh && ./run.sh
