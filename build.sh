8cf82112634944fe5e32bb93571433197215b5b5
Robert Pofuk <rpofuk@gmail.com>
2022-10-26 14:05:45 +0200

    Remove docker host stuff

--- build.sh
+++ build.sh
@@ -6,10 +6,14 @@ export TARGET_ACCOUNT_ID="$(aws sts get-caller-identity | jq -r '.Account')"
 export DOCKER_BUILDKIT=0
 export DOCKER_BUILDKIT=1
 
-export TARGET=ubuntu@${1}
-if [[ "${1:-}" != "" ]]; then
-  export DOCKER_HOST=ssh://${TARGET}
-fi
+export LASEST_IMAGE="$(aws ec2 describe-images \
+                          --owners self --no-paginate  \
+			  | jq -r '.Images[].Name' \
+			  | grep build-alpha-jenkins  \
+			  | sort | tail -1)"
+
+echo "Last image found: $LASEST_IMAGE"
+
 export BUILD_ID="74"
 
 export AWS_DEFAULT_REGION=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
