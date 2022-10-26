b94c4dc669dfed5a7136d991c484ac92f4b67b04
Robert Pofuk <rpofuk@gmail.com>
2022-10-26 14:14:07 +0200

    Automate image number detection

--- build.sh
+++ build.sh
@@ -6,15 +6,22 @@ export TARGET_ACCOUNT_ID="$(aws sts get-caller-identity | jq -r '.Account')"
 export DOCKER_BUILDKIT=0
 export DOCKER_BUILDKIT=1
 
-export LASEST_IMAGE="$(aws ec2 describe-images \
+export LATEST_IMAGE="$(aws ec2 describe-images \
                           --owners self --no-paginate  \
 			  | jq -r '.Images[].Name' \
 			  | grep build-alpha-jenkins  \
 			  | sort | tail -1)"
 
-echo "Last image found: $LASEST_IMAGE"
+echo "Last image found: $LATEST_IMAGE"
 
-export BUILD_ID="74"
+if [[ "$LATEST_IMAGE" == "" ]]; then
+  export BUILD_ID="0"
+else
+  export BUILD_ID="${LATEST_IMAGE##*.b}"
+fi
+export BUILD_ID=$((BUILD_ID+1))
+echo "New build id: $BUILD_ID"
+
 
 export AWS_DEFAULT_REGION=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
 
