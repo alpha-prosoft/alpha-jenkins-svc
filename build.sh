83da5c79e76ca5451f355f07c59ed9c3a5a6b0b5
Robert Pofuk <rpofuk@gmail.com>
2022-10-26 13:52:36 +0200

    Copy over all config

--- build.sh
+++ build.sh
@@ -2,9 +2,17 @@
 
 set -e 
 
+export TARGET_ACCOUNT_ID="$(aws sts get-caller-identity | jq -r '.Account')"
+export DOCKER_BUILDKIT=0
+export DOCKER_BUILDKIT=1
 
-echo "Building alpha-jenkins-svc ${BUILD_ID}"
+export TARGET=ubuntu@${1}
+if [[ "${1:-}" != "" ]]; then
+  export DOCKER_HOST=ssh://${TARGET}
+fi
+export BUILD_ID="74"
 
+export AWS_DEFAULT_REGION=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
 
 docker build --progress=plain \
              --no-cache \
