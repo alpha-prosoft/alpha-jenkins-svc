6bcdd8dbba84f4d3abff360fd3c91d5af46d39cd
Robert Pofuk <rpofuk@gmail.com>
2024-04-13 08:45:14 +0200

    Move deploy.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,9 @@ set -e
 
 echo "Going to deploy me some stuff"
 
-curl https://raw.githubusercontent.com/raiffeisenbankinternational/cbd-jenkins-pipeline/master/ext/deploy.sh > deploy.sh
-chmod +x deploy.sh
-
-
+target_dir=$(mktemp -d)
+curl https://raw.githubusercontent.com/raiffeisenbankinternational/cbd-jenkins-pipeline/master/ext/deploy.sh > $target_dir/deploy.sh
+chmod +x $target_dir/deploy.sh
 
 export TARGET_ACCOUNT_ID="$(aws sts get-caller-identity | jq -r '.Account')"
 
@@ -16,5 +15,5 @@ docker run -v /var/run/docker.sock:/var/run/docker.sock \
 	  -e EnvironmentNameUpper="PIPELINE" \
 	  -e ServiceName="alpha-jenkins-svc" \
 	  -e BUILD_ID="${BUILD_ID}" \
-	  -v $PWD/deploy.sh:/dist/deploy.sh \
+	  -v $target_dir/deploy.sh:/dist/deploy.sh \
 	  alpha-jenkins-svc:b${BUILD_ID} /dist/deploy.sh
