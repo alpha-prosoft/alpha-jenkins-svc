74dccdcf1f19883c41ec2a819ee51296ae2f8378
Robert Pofuk <rpofuk@gmail.com>
2024-12-09 09:37:35 +0100

    Fix target dir

--- run.sh
+++ run.sh
@@ -4,10 +4,11 @@ set -e
 
 echo "Going to deploy me some stuff"
 
-target_dir=$(mktemp -d)
+target_dir=${PWD}/target
+mkdir -p ${target_dir}
 curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/raiffeisenbankinternational/cbd-jenkins-pipeline/master/ext/deploy.sh > $target_dir/deploy.sh
 chmod +x $target_dir/deploy.sh
-
+chmod 755 $target_dir/deploy.sh
 export TARGET_ACCOUNT_ID="$(aws sts get-caller-identity | jq -r '.Account')"
 
 docker run -v /var/run/docker.sock:/var/run/docker.sock \
