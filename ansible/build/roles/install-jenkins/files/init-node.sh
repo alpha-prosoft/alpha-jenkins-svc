018d9ea9c7a08292164558a145904c05ae0cf4b6
Robert Pofuk <rpofuk@gmail.com>
2024-12-30 18:10:27 +0100

    Fix init node

--- ansible/build/roles/install-jenkins/files/init-node.sh
+++ ansible/build/roles/install-jenkins/files/init-node.sh
@@ -6,8 +6,9 @@ source /etc/environment
 mkdir -p /home/jenkins/.ssh
 
 aws ssm get-parameter \
-        --name "/pipeline/public/jenkins/.ssh/id_rsa.pub" \
+        --name "/${EnvironmentNameLower}/keys/public/jenkins/public-key" \
         --with-decryption \
         --query 'Parameter.Value' \
         --output text | base64 -d > /home/jenkins/.ssh/authorized_keys
+
 chown jenkins:jenkins /home/jenkins/.ssh
