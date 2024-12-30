ea12159b024e3505d9fb1b0dce167163b3694291
pofuk <pofuk@walter-group.com>
2024-12-30 17:54:36 +0100

    Fix names

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -9,12 +9,15 @@ target_dir=${PWD}/target
 mkdir -p $target_dir 
 
 curl -H 'Cache-Control: no-cache' \
-        https://raw.githubusercontent.com/raiffeisenbankinternational/cbd-jenkins-pipeline/master/ext/deploy.sh \
+        https://raw.githubusercontent.com/raiffeisenbankinternational/cbd-jenkins-pipeline/master/ext/build.sh \
 	> $target_dir/build.sh
 
 curl -H 'Cache-Control: no-cache' \
-        https://raw.githubusercontent.com/raiffeisenbankinternational/cbd-jenkins-pipeline/master/ext/deploy.sh \
+        https://raw.githubusercontent.com/raiffeisenbankinternational/cbd-jenkins-pipeline/master/ext/run.sh \
 	> $target_dir/run.sh
 
-${target_dir}/build.sh ${PROJECT_NAME} "jenkins-svc" "${ENV_NAME_UPPER}"
-${target_dir}/run.sh ${PROJECT_NAME} "jenkins-svc" "${ENV_NAME_UPPER}"
+chmod +x ${target_dir}/build.sh
+chmod +x ${target_dir}/run.sh
+
+source ${target_dir}/build.sh ${PROJECT_NAME} "jenkins" "${ENV_NAME_UPPER}"
+source ${target_dir}/run.sh ${PROJECT_NAME} "jenkins" "${ENV_NAME_UPPER}"
