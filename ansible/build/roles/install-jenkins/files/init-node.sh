d1a53eed97e0d543913ad259141165fcdb31cfb2
pofuk <pofuk@walter-group.com>
2026-01-02 17:09:55 +0100

    Update ssh-key path

--- ansible/build/roles/install-jenkins/files/init-node.sh
+++ ansible/build/roles/install-jenkins/files/init-node.sh
@@ -6,9 +6,9 @@ source /etc/environment
 mkdir -p /home/jenkins/.ssh
 
 aws ssm get-parameter \
-        --name "/${EnvironmentNameLower}/keys/public/jenkins/public-key" \
-        --with-decryption \
-        --query 'Parameter.Value' \
-        --output text | base64 -d > /home/jenkins/.ssh/authorized_keys
+  --name "/${EnvironmentNameLower}/public/.ssh/id_rsa.pub" \
+  --with-decryption \
+  --query 'Parameter.Value' \
+  --output text | base64 -d >/home/jenkins/.ssh/authorized_keys
 
 chown jenkins:jenkins /home/jenkins/.ssh
