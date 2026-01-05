00dc78fe353b1a9d704c1e713456d37e0d62d668
pofuk <pofuk@walter-group.com>
2026-01-05 11:22:47 +0100

    Fix buuild

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-set -euo pipefail 
+set -euo pipefail
 
 export PROJECT_NAME=${1}
 export ENV_NAME_UPPER=${2:-PIPELINE}
 
 target_dir=${PWD}/target
-mkdir -p $target_dir 
+mkdir -p $target_dir
 
 curl -H 'Cache-Control: no-cache' \
-        https://raw.githubusercontent.com/raiffeisenbankinternational/cbd-jenkins-pipeline/master/ext/build.sh \
-	> $target_dir/build.sh
+  https://raw.githubusercontent.com/alpha-prosoft/cbd-jenkins-pipeline/master/ext/build.sh \
+  >$target_dir/build.sh
 
 curl -H 'Cache-Control: no-cache' \
-        https://raw.githubusercontent.com/raiffeisenbankinternational/cbd-jenkins-pipeline/master/ext/run.sh \
-	> $target_dir/run.sh
+  https://raw.githubusercontent.com/alpha-prosoft/cbd-jenkins-pipeline/master/ext/run.sh \
+  >$target_dir/run.sh
 
 chmod +x ${target_dir}/build.sh
 chmod +x ${target_dir}/run.sh
 
 source ${target_dir}/build.sh ${PROJECT_NAME} "jenkins" "${ENV_NAME_UPPER}"
-source ${target_dir}/run.sh ${PROJECT_NAME} "jenkins" "${ENV_NAME_UPPER}"
